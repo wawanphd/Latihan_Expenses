00e6e1c6aad3fab60b8f78d441a46bc0685311fb
wawanphd <gunawanwawan15@gmail.com>
2024-11-29 09:04:49 +0700

    add expenses

--- pengeluaran/urls.py
+++ pengeluaran/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import index, expenses, add_expenses, reports_expenses
+from .views import index, expenses, add_expenses, reports_expenses, save_expenses
 
 urlpatterns = [
     path('', index, name='index'),
     path('expenses/', expenses, name='expenses'),
     path('add-expenses/', add_expenses, name='add-expenses'),
     path('reports-expenses/', reports_expenses, name='reports-expenses'),
+
+    path('save_expenses/', save_expenses, name='save_expenses'),
+
 ]
--- pengeluaran/views.py
+++ pengeluaran/views.py
@@ -1,8 +1,26 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Expenses
 
 
+def save_expenses(request):
+    #Ambil data dari form add-expenses.html
+    tanggal = request.POST.get('date')
+    kategori = request.POST.get('category')
+    deskripsi = request.POST.get('description')
+    jumlah = request.POST.get('amount')    
+    
+    #Proses Simpan kedalam database (model expenses =>tabel pengeluaran_expenses)
+    simpan = Expenses(
+        date= tanggal,
+        category = kategori,
+        description = deskripsi,
+        amount = jumlah
+    )
+    simpan.save()
+
+
+    return redirect('expenses')
 
 def index(request):
 
