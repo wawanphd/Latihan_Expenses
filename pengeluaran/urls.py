0243d378ac4b871b6ae9be5aae43c6ce495ed806
wawanphd <gunawanwawan15@gmail.com>
2024-12-09 07:03:01 +0700

    form update and delete

--- pengeluaran/urls.py
+++ pengeluaran/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import index, expenses, add_expenses, reports_expenses, save_expenses
+from .views import index, expenses, add_expenses, reports_expenses, save_expenses, delete_expenses, form_update_expenses
 
 urlpatterns = [
     path('', index, name='index'),
@@ -8,6 +8,13 @@ urlpatterns = [
     path('add-expenses/', add_expenses, name='add-expenses'),
     path('reports-expenses/', reports_expenses, name='reports-expenses'),
 
+    #path untuk save expenses
     path('save_expenses/', save_expenses, name='save_expenses'),
+
+    #path untuk delete expenses
+    path('delete_expenses/<int:id>', delete_expenses, name='delete_expenses'),
+
+    #path untuk form edit expenses
+    path('form_update_expenses/<int:id>', form_update_expenses, name='form_update_expenses'),
 
 ]
--- pengeluaran/views.py
+++ pengeluaran/views.py
@@ -3,14 +3,54 @@ from django.shortcuts import render,redirect
 from .models import Expenses
 
 
+def form_update_expenses(request, id):
+    
+    #ambil data id yang akan diedit
+    data_edit = Expenses.objects.get(id=id)
+    
+    context ={
+        'data_edit':data_edit
+    }
+
+
+
+    return render(request, 'edit-expenses.html', context)
+
+
+
+
+
+#fungsi Hapus
+def delete_expenses(request, id):
+    print(id)
+
+    #cari id yang akan dihapus
+    id_hapus = Expenses.objects.get(id=id)
+
+    #lakukan fungsi hapus berdasarkan id_hapus
+    id_hapus.delete()
+
+
+    return redirect('expenses')
+
+
+
+
+
+
+
+
 def save_expenses(request):
     #Ambil data dari form add-expenses.html
     tanggal = request.POST.get('date')
     kategori = request.POST.get('category')
     deskripsi = request.POST.get('description')
-    jumlah = request.POST.get('amount')    
+    jumlah = request.POST.get('amount') 
+
+    
     
     #Proses Simpan kedalam database (model expenses =>tabel pengeluaran_expenses)
+    # Perintah ini sama dengan perintah insert 
     simpan = Expenses(
         date= tanggal,
         category = kategori,
